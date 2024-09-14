1c944f056b44d3886c7ab4a69a3ee7ee0ee5035f
luuhaohiep1218 <luuhaohiep8a@gmail.com>
2024-09-14 16:43:03 +0700

    Truyen props cha -> con loaddata vao table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,35 @@ import Link from "next/link";
 import { Container } from "react-bootstrap";
 import AppTable from "@/components/app.table";
 import { useEffect } from "react";
+import useSWR from "swr";
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log("check res:", data);
-    };
-    fetchData();
-  }, []);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+  if (!data) return <div>Loading ...</div>;
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log("check res:", data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <Container>
-      <AppTable />
+      <AppTable blogs={data} />
     </Container>
   );
 }
--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -1,34 +1,39 @@
-import Table from "react-bootstrap/Table";
+import { Button, Table } from "react-bootstrap";
 
-export default function AppTable() {
+interface IProps {
+  blogs: IBlog[];
+}
+export default function AppTable(props: IProps) {
+  const { blogs } = props;
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {blogs?.map((blog) => {
+          return (
+            <tr key={blog.id}>
+              <td>{blog.id}</td>
+              <td>{blog.title}</td>
+              <td>{blog.author}</td>
+              <td>
+                <Button variant="primary">View</Button>
+
+                <Button className="mx-3" variant="danger">
+                  Delete
+                </Button>
+                <Button variant="warning">Edit</Button>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
